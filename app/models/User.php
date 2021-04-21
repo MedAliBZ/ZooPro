b558a8c540f5d07e2617fc8003004c19c2ef954f
MohamedAliBouzaiene <mohamedali.bouzaiene@esprit.tn>
2021-04-21 16:52:30 +0100

    fixed update bug

--- app/models/User.php
+++ app/models/User.php
@@ -137,6 +137,10 @@ class User
     }
 
     public function update($data){
+        $this->db->query('DELETE FROM password_reset WHERE password_reset.username = (SELECT username FROM users WHERE id = :id)');
+        $this->db->bind(':id', $_SESSION["id"]);
+        $this->db->execute();
+
         $this->db->query('UPDATE users SET username = :username , email = :email WHERE id = "'.$_SESSION["id"].'" ');
         //Bind values
         $this->db->bind(':username', $data['username']);
